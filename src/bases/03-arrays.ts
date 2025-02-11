128a154e52dbaa5bb3454ba5384a596bb585f09e
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-02-11 19:44:14 +0100

    funciones y objecto

--- src/bases/03-arrays.ts
+++ src/bases/03-arrays.ts
@@ -7,14 +7,16 @@ const numberArray2 = [ ...numberArray ];
 
 //como adicionar un elemento mas en la lista
 numberArray2.push(7);
-//console.log(numberArray);
-//console.log(numberArray2;)
+console.log(numberArray);
+console.log(numberArray2);
 
-//COMO HACER UN MAPEADO. con una funcion de flecha y propiedad .map  CREAR OTRA LISTA TRANSFORMANDO TODOS LOS ELEMENTOS DE LA LISTA
+//COMO HACER UN MAPEADO.
+// una mapeado hace una lista de una lista- transformando los elementos de la lista inicial
+//  con una funcion de flecha y propiedad .map  CREAR OTRA LISTA TRANSFORMANDO TODOS LOS ELEMENTOS DE LA LISTA
 //const numberArray3 = numberArray.map( (valor) => {
  //   return (valor * 2.455).toFixed(1)
  //} )
  // las funciones de flecha se pueden hacer mas sencillas quitando el return y las flechas
- const numberArray3 = numberArray.map(valor=> (valor*2.345).toFixed(1));
+ const numberArray3 = numberArray.map( (valor)=> (valor * 2.345).toFixed(1));
  console.log(numberArray3);
 
