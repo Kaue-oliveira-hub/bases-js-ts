397eabd74f6891092a2d04dbbc6e156759d00213
Kaue-oliveira-hub <kaueoliveiramcz@gmail.com>
2025-02-13 23:08:49 +0100

    fetch a una api

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,10 @@ import viteLogo from '/vite.svg'
 //import './bases/05-des-obj'
 //import './bases/06-deses-arrays'
 //import './bases/07-imp-exp'
-import './bases/08-promises'
+//import './bases/08-promises'
+import './bases/09-fetch-api'
+
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
